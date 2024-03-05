f56cd0a69c70b27179e05350e5c50e606fe0d1c6
cbum-dev <priyanshukumar2002234@gmail.com>
2024-03-05 10:46:23 +0530

    COntact Us Sorted

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
@@ -13,23 +12,24 @@ import { faHashnode } from "@fortawesome/free-brands-svg-icons/faHashnode";
 const Contact = () => {
   return (
     <div className="text-white  flex flex-col items-center mx-4 justify-center">
-      <h1 className="text-3xl md:text-6xl lg:text-7xl my-4">Contact Us</h1>
-      <div className="bg-zinc-950 w-full rounded-lg py-6 md:px-6 px-4">
-        <h1 className="md:text-3xl text-2xl underline text-center text-blue-300 mt-2 mb-2">
+      <h1 className="text-3xl md:text-6xl lg:text-7xl my-4 underline mb-6">Contact Us</h1>
+      <div className="">
+        <div className="bg-zinc-900  w-full rounded-lg py-6 md:px-6 px-4">
+        <h1 className=" text-2xl underline text-center text-blue-300 mt-2 mb-2">
           Contact Us: Contribute to Open Source with Fix It!
         </h1>
 
-        <p className="md:text-xl text-lg text-center">
+        <p className=" text-lg text-center">
           Welcome to Fix It your gateway to impactful contributions in the world
           of open-source development. At Fix It, we believe in the power of
           collaboration, and we invite you to be part of our vibrant community.
         </p>
       </div>
-      <div className="bg-zinc-950 rounded-lg w-full py-6 md:px-6 px-4 my-2">
+      <div className="bg-zinc-900 rounded-lg w-full py-6 md:px-6 px-4 my-2">
         <h1 className="md:text-3xl text-2xl underline text-center text-blue-300 mt-2 mb-2">
           Why Contribute?
         </h1>
-        <ul className="md:text-xl text-lg text-left ">
+        <ul className="md:text-lg  text-center ">
           <li className="">
             1. Skill Growth: Enhance your skills by working on real-world
             projects. Whether you're a seasoned developer or just starting,
@@ -46,22 +46,22 @@ const Contact = () => {
           </li>
         </ul>
       </div>
-      <div className="bg-zinc-950 rounded-lg w-full py-6 md:px-6 px-4 mb-2">
+      <div className="bg-zinc-900 rounded-lg w-full py-6 md:px-6 px-4 mb-2">
         <h1 className="md:text-3xl text-2xl underline text-center text-blue-300 mt-2 mb-2">
           How to Contribute:
         </h1>
-        <p className="md:text-xl text-lg text-center">
+        <p className=" text-lg text-center">
           Visit our GitHub repository to explore ongoing projects, find issues
           to work on, and make meaningful contributions. We're continuously
           updating our projects, and your expertise can make a significant
           impact.
         </p>
       </div>
-      <div className="bg-zinc-950 rounded-lg w-full py-6 md:px-6 px-4 mb-2">
+      <div className="bg-zinc-900 rounded-lg w-full py-6 md:px-6 px-4 mb-2">
         <h1 className="md:text-3xl text-2xl underline text-center text-blue-300 mt-2 mb-2">
           Get in Touch
         </h1>
-        <p className="md:text-xl text-lg text-center">
+        <p className=" text-lg text-center">
           Have questions or ideas to share? Want to discuss potential
           collaborations? Reach out to us via email at
           contact@fixitopensource.com or connect with us on Twitter and
@@ -70,7 +70,7 @@ const Contact = () => {
         </p>
       </div>
 
-      <div className="bg-zinc-950 rounded-lg py-6 md:px-6 px-4 mb-2 w-full">
+      <div className="bg-zinc-900 rounded-lg py-6 md:px-6 px-4 mb-2 w-full">
         <ul className="flex justify-evenly space-x-4 mt-4 ">
           <li className="mb-4">
             <a
@@ -140,6 +140,8 @@ const Contact = () => {
           </li>
         </ul>
       </div>
+      </div>
+      
     </div>
   );
 };
